caa897c5bb3e5704b2f45c8aac3270ce184f0761
Nathan Nuckels <nathannuckels@gmail.com>
2021-01-03 01:04:07 -0600

    Added debug messages for loading a save.

--- dungeon.py
+++ dungeon.py
@@ -10,3 +10,18 @@ class Game:
 		self.savePath=""
 	def setSave(self,save):
 		self.savePath=save		
+		file=[]
+		with open(save,"r") as f:
+			for line in f:
+				file.append(line.strip())
+		self.day=int(file[0])
+		self.hour=int(file[1])
+		self.minute=int(file[2])
+		self.name=file[3]
+		self.money=int(file[4])
+		self.location=file[5]
+		self.inventory=eval(file[6])
+		print("Day is "+str(self.day)+", TIme is "+str(self.hour)+":"+str(self.minute))
+		print(self.name.upper()+" at "+self.location+" with $"+str(self.money))
+		print("Holding:")
+		print(self.inventory)
